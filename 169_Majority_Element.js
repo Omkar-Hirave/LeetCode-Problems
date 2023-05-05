2352403dae2e00078b631ac3ab051816ac24a605
oi-OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-05-05 09:46:29 +0530

    Create 169_Majority_Element.js

---
+++ 169_Majority_Element.js
@@ -0,0 +1,13 @@
+function majorityElement(nums){
+  let count = 0 , candidate = 0 
+  for(let i = 0 ; i < nums.length ; i++){
+    if(count === 0 ){
+      candidate = nums[i]
+    }if(nums[i] === candidate){
+        count++
+    }else {
+        count--
+    }
+  }return candidate
+}
+  
