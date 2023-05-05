cfb6d0bc5cb22f3569ed976316b500679c6f19d0
oi-OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-05-05 10:45:40 +0530

    Create 1_Two_Sum.js

---
+++ 1_Two_Sum.js
@@ -0,0 +1,11 @@
+function twoSum(nums,target){
+    let map = new Map()
+    for (let i = 0 ; i < nums.length ; i++){
+        let complement = target - nums[i]
+        if(map.has(complement)){
+            return [map.get(complement),i]
+         }
+         map.set(nums[i],i)
+     }
+      return []
+}
