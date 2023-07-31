5aa2dad02be8f406a5744697cf6423c5e75c27a1
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-07-31 08:11:47 +0530

    Add files via upload

---
+++ 2798_NumberofEmployees .java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] nums, int target) {
+        int count = 0 ;
+        for(int i = 0 ; i < nums.length ;i++){
+            if(nums[i] >= target )
+                count++;
+        }
+        return count;
+    }
+}
