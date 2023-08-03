762dc2a812e37d17256fed70e8e33d32a922e479
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-08-03 10:46:20 +0530

    Added another solution similar to finding second smallest element in an array with O(n) time complexity

--- 2706_BuyTwoChocolates.java
+++ 2706_BuyTwoChocolates.java
@@ -1,10 +1,31 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-       Arrays.sort(prices);
-       int chocolate = money - (prices[0] + prices[01]);
-       if(chocolate >= 0){
-           return chocolate;
-       }
-       return money;
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] <= smallest) {
+                secondSmallest = smallest;
+                smallest = prices[i];
+            } else if (prices[i] <= secondSmallest && prices[i] >= smallest) {
+                secondSmallest = prices[i];
+            }
+        }
+        
+        int chocolate = money - (smallest + secondSmallest);
+        if (chocolate >= 0) {
+            return chocolate;
+        }
+        return money;
     }
 }
+// class Solution {
+//     public int buyChoco(int[] prices, int money) {
+//        Arrays.sort(prices);
+//        int chocolate = money - (prices[0] + prices[01]);
+//        if(chocolate >= 0){
+//            return chocolate;
+//        }
+//        return money;
+//     }
+// }
