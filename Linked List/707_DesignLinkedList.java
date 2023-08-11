30b5eb7339d1d5652ef3e29e9cf798eae0fcbd87
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-08-11 08:39:08 +0530

    Add files via upload

---
+++ Linked List/707_DesignLinkedList.java
@@ -0,0 +1,86 @@
+public class MyLinkedList {
+    private ListNode head;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+    
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size || head == null)
+            return -1;
+
+        ListNode current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.val;
+    }
+
+    public void addAtHead(int val) {
+        ListNode newNode = new ListNode(val);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    public void addAtTail(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size)
+            return;
+
+        if (index == 0) {
+            addAtHead(val);
+        } else if (index == size) {
+            addAtTail(val);
+        } else {
+            ListNode newNode = new ListNode(val);
+            ListNode current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+            size++;
+        }
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size)
+            return;
+
+        if (index == 0) {
+            head = head.next;
+        } else {
+            ListNode current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+        size--;
+    }
+}
