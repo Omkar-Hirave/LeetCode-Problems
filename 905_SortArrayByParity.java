43498b486f2b1bd3464e1bd5fedf31495602b814
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-08-29 07:00:43 +0530

    Add files via upload

--- 905_SortArrayByParity.java
+++ 905_SortArrayByParity.java
@@ -1,16 +1,23 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int arr[] = new int[nums.length];
-        int k = 0 , l = nums.length-1;
-        for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] % 2 == 0){
-                arr[k] = nums[i];
-                k++;
+        int n = nums.length;
+        int low = 0 , high = n - 1;
+        while(low < high){
+            if(nums[low] % 2 == 0){
+                low++;
+            }else if(nums[high] % 2 == 0){
+                swap(nums , low , high);
+                low++;
+                high--;
             }else{
-                arr[l] = nums[i];
-                l--;
+                high--;
             }
         }
-        return arr;
+        return nums;
+    }
+    public void swap(int nums[] , int i , int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
