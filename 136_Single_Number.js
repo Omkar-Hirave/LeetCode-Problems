b05390d6c0094e63918a0ce13f5d59a5ed982db4
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-09-03 07:56:39 +0530

    Delete 136_Single_Number.js to add java file

--- 136_Single_Number.js
+++
@@ -1,9 +0,0 @@
-function singleNumber(nums){
-    let result = 0 
-    for(let i = 0 ; i < nums.length ; i++){
-        result = result ^ nums[i]
-    }
-    return result
-}
-let nums = [7,4,4,1,2,1,2]
-console.log(singleNumber(nums));
