d36108e011711aed8046158e8dcd056053f71da9
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-09-03 09:49:38 +0530

    Add files via upload

---
+++ 215_Kth_Largest_Element_Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0 ; i < k ; i++){
+            pq.offer(nums[i]);
+        }
+        for(int i = k ; i < nums.length ; i++){
+            if(nums[i] > pq.peek()){
+                pq.poll();
+                pq.offer(nums[i]);
+            }
+        }
+        return pq.peek();
+    }
+}
