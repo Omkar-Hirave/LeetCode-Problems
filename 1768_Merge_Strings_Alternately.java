0e34929389c1b425a9906b5e077b7f0749ef5852
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-09-08 20:03:07 +0530

    Add files via upload

---
+++ 1768_Merge_Strings_Alternately.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+      StringBuilder sb = new StringBuilder();
+      int i = 0 , j = 0 ;
+      while(i < word1.length() || j < word2.length()){
+          if( i < word1.length()){
+            sb.append(word1.charAt(i));
+            i++;
+          }
+          if(j < word2.length()){
+            sb.append(word2.charAt(j));
+            j++;
+          }
+      }  
+      return sb.toString();
+    }
+}
