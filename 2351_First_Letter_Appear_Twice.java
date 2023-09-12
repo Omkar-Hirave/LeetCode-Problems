ffd31f3cf5984460cc1b99eb57a49d45b4d81adc
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-09-12 14:38:14 +0530

    Add files via upload

--- 2351_First_Letter_Appear_Twice.java
+++ 2351_First_Letter_Appear_Twice.java
@@ -1,6 +1,7 @@
 class Solution {
     public char repeatedCharacter(String s) {
         HashSet<Character> set = new HashSet<>();
+        // This problems has  O(1) space complexity since we can have only atmost 25 characters in the set  
         for(char ch : s.toCharArray()){
             if(set.contains(ch))
                 return ch;
