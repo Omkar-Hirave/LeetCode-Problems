d3b1af109255490bd2bbe1c68bb9021ba70470f8
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-09-12 21:43:42 +0530

    Add files via upload

---
+++ 125_Valid_Palindrome.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        int left = 0 , right = s.length()-1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;                
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
