a5e9e33491f39a79ae517ce8200e5fda7fee05f4
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-09-15 09:05:25 +0530

    Add files via upload

---
+++ 169_Majority_Element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0 , candidate = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(count == 0){
+                candidate = nums[i];
+            }
+            if(candidate == nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
